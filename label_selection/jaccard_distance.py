8d70b5cf39f10eeb064c91d29bd564e63be0e5fe
SeoKwangWook <100064247+tommyEzreal@users.noreply.github.com>
2023-03-18 18:34:06 +0900

    Create jaccard_distance.py

---
+++ label_selection/jaccard_distance.py
@@ -0,0 +1,15 @@
+from konlpy import *
+mecab = Mecab()
+
+# tokenize by morpheme-level 
+def jaccard_distance(sentence_pair):
+    mecab = Mecab()
+    tokens1 = set(mecab.morphs(sentence_pair[0]))
+    tokens2 = set(mecab.morphs(sentence_pair[1]))
+    
+    # Calculate the Jaccard distance between the two sets of morphemes
+    jaccard_distance = len(tokens1.intersection(tokens2)) / len(tokens1.union(tokens2))
+
+    return jaccard_distance
+  
+  
