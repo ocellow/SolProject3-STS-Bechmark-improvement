45c7b2ee6868316b41319ee77eff279750c21e5b
SeoKwangWook <100064247+tommyEzreal@users.noreply.github.com>
2023-03-20 23:53:45 +0900

    split train_utils from FindRelabelTarget

--- label_selection/find_relabel_target.py
+++ label_selection/find_relabel_target.py
@@ -14,7 +14,9 @@ import logging
 from importlib import reload
 
 from cross_encoder.cross_encoder import CrossEncoder
+from cross_encoder import train_utils
 from model_evaluation.model_evaluator import ModelEvaluator
+ 
 
 
 # 학습경과 모니터링하는 logger 초기화
@@ -26,22 +28,29 @@ logging.basicConfig(
     handlers=[LoggingHandler()],
 )
 
-class FindRelabelTarget():
-    def __init__(self, num_epochs, train_batch_size, model_save_path, train_path, dev_path, num_folds=10):
-        
+class FindRelabelTarget(): # Trainer 상속하게 만들기 
+    def __init__(self,
+                model_save_path, 
+                train_path,
+                dev_path,
+                num_epochs=2,
+                train_batch_size=64,
+                num_folds=10):
+
         self.num_epochs = num_epochs
         self.train_batch_size = train_batch_size
         self.model_save_path = model_save_path
-
-        self.og_train = pd.read_csv(train_path)
-        self.og_train = self.og_train.reset_index() ### 
-        self.og_train = self.og_train[['sentence1','sentence2','labels.label']] # train_path = og_train_path ###
-        self.dev = pd.read_csv(dev_path) # dev_path = KLUE devset_path
-        self.num_folds = num_folds
         
+        self.og_train = pd.read_csv(train_path)
+        self.og_train = self.og_train.reset_index()
+        self.og_train = self.og_train[['sentence1','sentence2','labels.label']]
+        
+        self.dev = pd.read_csv(dev_path)
+        self.dev = self.dev[['sentence1','sentence2','labels.label']]
+        
+        self.num_folds = num_folds
         self.filtered_df = self.filter_df_by_label_range()
         
-
     
     # train 에서 2.0-3.5값만 추출한 데이터프레임 만들기 
     def filter_df_by_label_range(self):
@@ -78,73 +87,6 @@ class FindRelabelTarget():
 
         return train_data, test_data
 
-    # trainset에서 devset분포에 맞게 valset 추출 
-    def extract_val_from_train(self, train_data):
-        dev = self.dev[['sentence1','sentence2','labels.label']]
-        
-        # select val by label dist 
-        val_ratio = (dev.groupby('labels.label').size() / dev.groupby('labels.label').size().sum()).to_frame('ratio')
-        val_ratio['number'] = round(val_ratio['ratio'] * (len(train_data) *0.05)).astype(int) # train데이터의 5%를 val로 사용 
-        
-        val_data = pd.DataFrame()
-        label = list(set(train_data['labels.label'].values))
-        
-        
-        for i in label:
-            sample = train_data.loc[train_data['labels.label']==i].sample(n=val_ratio['number'][i], random_state = 0)
-            val_data = pd.concat([val_data,sample])
-        
-        
-        train_data = train_data.drop(val_data.index)
-        return train_data, val_data
-    
-    # train, val 한번에 input_examples로 변환 
-    def create_input_examples(self,train,val):
-
-        dfs = [train, val]
-        input_examples = [[] for _ in range(2)]
-
-        for i, df in enumerate(dfs):
-            for _, row in tqdm(df.iterrows()):
-                sentence1, sentence2, score = row[0], row[1], row[2]/5.0
-                input_examples[i].append(InputExample(texts=[sentence1, sentence2], label=score))
-
-        return input_examples[0], input_examples[1] # train, val
-
-
-    def train(self, pretrained_model_name, model_save_path, train):
-        # load pretrained model 
-        cross_encoder = CrossEncoder(pretrained_model_name, num_labels=1)
-
-        logging.info('extract_val_from_train_data')
-        train, val = self.extract_val_from_train(train) # extract val from train 
-
-        logging.info('create_input_examples')
-        sts_train_examples, sts_val_examples = self.create_input_examples(train,val)
-
-        logging.info('assign_DataLoader_and_val_evaluator')
-        train_dataloader = DataLoader(sts_train_examples,
-                                      shuffle=True,
-                                      batch_size = self.train_batch_size)
-        val_evaluator = ModelEvaluator.from_input_examples(sts_val_examples)
-        
-        warmup_steps = math.ceil(len(train_dataloader) * self.num_epochs / self.train_batch_size*0.1) # 10%of train
-
-        logging.info(f'start train model..')
-        cross_encoder.fit(
-            train_dataloader = train_dataloader,
-            evaluator=val_evaluator,
-            epochs=self.num_epochs,
-            evaluation_steps=int(len(train_dataloader)*0.1),
-            optimizer_params = {'lr':5e-5},
-            warmup_steps=warmup_steps,
-            output_path=model_save_path,
-        )
-
-        return cross_encoder
-
-
-
     def extract_false(self, pretrained_model_name=None):
     # fold num에 따라 데이터 선택 
         
@@ -171,24 +113,30 @@ class FindRelabelTarget():
             
             # 학습 루프마다 다른 이름으로 저장 
             model_save_path = self.model_save_path + '-' + f'{i+1}th fold'
-
+            
+            # [i]th train,val,test 
+            logging.info('get_data_by_fold / extract_test')
+            train, test = self.get_data_by_fold(i) # extract test from train(2.0-3.5)
+            
             # load & initialize pretrained model 
             if i ==0:
                 logging.info(f'load pretrained_model:{pretrained_model_name}..')
             else: 
                 logging.info(f'model initialization:{i+1}th..')
             
-            # [i]th train,val,test 
-            logging.info('get_data_by_fold / extract_test')
-            train, test = self.get_data_by_fold(i) # extract test from train(2.0-3.5)
-
             # load pretrained model / train model by each fold 
-            cross_encoder = self.train(pretrained_model_name, model_save_path, train)
-            
+            cross_encoder = train_ce(model_save_path,
+                                     train,
+                                     self.dev,
+                                     self.num_epochs,
+                                     self.train_batch_size,
+                                     pretrained_model_name,
+                                     verbose=False)
+
             # predict test data / return label 
             logging.info(f'extract False Label..')
             test_pairs = list(zip(test['sentence1'], test['sentence2']))
-            pred = cross_encoder.predict(test_pairs, show_progress_bar=True)
+            pred = cross_encoder.predict(test_pairs)
             pred = np.round([num * 5.0 for num in pred],1) # 0-5단위 점수로 복구
 
             # Flase Negative and False Positive label Extraction 
@@ -245,12 +193,18 @@ class FindRelabelTarget():
                 model_save_path = self.model_save_path + '-' + f'{pretrained_model_name}' + '-' + f'seed{seed}'
                 
                 # load pretrained model / train with each random seed  
-                cross_encoder = self.train(pretrained_model_name, model_save_path, train)
+                cross_encoder = train_ce(model_save_path,
+                                         train,
+                                         self.dev,
+                                         self.num_epochs,
+                                         self.train_batch_size,
+                                         pretrained_model_name,
+                                         verbose=False)
             
                 # predict test data / return label 
                 logging.info(f'Do test..')
                 test_pairs = list(zip(test['sentence1'], test['sentence2']))
-                pred = cross_encoder.predict(test_pairs, show_progress_bar=True)
+                pred = cross_encoder.predict(test_pairs)
                 pred = np.round([num * 5.0 for num in pred],1) # 0-5단위 점수로 복구
                 
                 preds_all_model.append(pred)
@@ -267,21 +221,19 @@ class FindRelabelTarget():
 
         return uncertain_dp_idx
 
-
 if __name__ == '__main__':
     num_epochs = 1
     train_batch_size = 64
     model_save_path = '/content/sample_data/cross_encoder'
     train_path = 'https://raw.githubusercontent.com/tommyEzreal/SolProject3-STS-Bechmark-improvement/main/data/KLUE_STS_train%20(2).csv'
     dev_path = 'https://raw.githubusercontent.com/tommyEzreal/SolProject3-STS-Bechmark-improvement/main/data/KLUE_STS_val%20(2).csv'
-
-
-    find_re_target = FindRelabelTarget(num_epochs,
-                                       train_batch_size,
-                                       model_save_path,
-                                       train_path,
-                                       dev_path,
-                                       num_folds=2)
     
+    find_re_target = FindRelabelTarget(model_save_path = model_save_path,
+                                    train_path = train_path,
+                                    dev_path = dev_path,
+                                    train_batch_size = train_batch_size,
+                                    num_folds = 2)
+
     false_idx = find_re_target.extract_false()
     relabel_target = find_re_target.extract_uncertain(false_idx)
+    pd.read_csv(train_path).loc[relabel_target]
